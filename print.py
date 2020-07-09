af2dc13fef3db2e65dc6f3b8e1d1045c5f45b85b
Gabriel <gabospa@gmail.com>
2020-07-08 22:31:41 -0500

    Solutions to lists and recursivity

--- print.py
+++ print.py
@@ -36,7 +36,30 @@ class LinkedList:
                 break
             list_head = list_head.next
             cont += 1
-    
+
+    def insert_at_start(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete_node(self, position):
+        cont = 0
+        node = self.head
+        
+        if node is not None:
+            if position == -1:
+                self.head = node.next
+                node = None
+
+        while node:
+            if cont == position -1:
+                prev = node
+                node = node.next
+                prev.next = node.next
+                node = None
+                break
+            cont += 1
+
 
 
 if __name__ == '__main__':
@@ -47,9 +70,14 @@ if __name__ == '__main__':
         llist.insert_node(llist_item)
     
     llist.print_linked_list()
+    print('\n')
     new_position = int(input('Insert new position: '))
     new_data = int(input('Insert data for New Position: '))
     llist.insert_between(new_position, new_data)
     llist.print_linked_list()
-
-
+    print('\n')
+    new_start = int(input('Insert new start data: '))
+    llist.insert_at_start(new_start)
+    llist.print_linked_list()
+    llist.delete_node(int(input('Delete position: ')))
+    llist.print_linked_list()
